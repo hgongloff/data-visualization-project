8112127fe3254d8ec39d199bd03b3d725adb6cb4
Hunter <hgongloff12@gmail.com>
2022-01-26 14:30:55 -0500

    basic proof of concept

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import xlwings as xw
+
+
+wb = xw.Book('DummyData.xlsx')
+
+# Viewing available
+# sheets in it
+wks = xw.sheets
+print("Available sheets :\n", wks)
+  
+# Selecting a sheet
+ws = wks[0]
+  
+cities = ws.range("B3:B10").value
+
+
+# Selecting a value
+# from the selected sheet
+total_points = ws.range("S3:S10").value
+print("A value in sheet1 :", total_points)
+
+
+plt.bar(cities, total_points)
+plt.title('City vs Total Points Earned')
+plt.xlabel('City')
+plt.ylabel('Total Points Earned')
+plt.show()
+
+
+
