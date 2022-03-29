a56c7def8f8da2e5c019082b7f986d9ed636be32
Hunter <hgongloff12@gmail.com>
2022-03-29 14:44:58 -0400

    added bar and line graph functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 from tabnanny import filename_only
+from matplotlib.transforms import Bbox
 import xlwings as xw
 import matplotlib.pyplot as plt
 from math import remainder
@@ -17,15 +18,17 @@ import glob
 def read_saved_data():
     saved_dataframes = []
     file_names = []
+
     path = r'static/stored-data'  # use your path
     all_files = glob.glob(path + "/*.csv")
     for filename in all_files:
         df = pd.read_csv(filename, index_col=None, header=0)
+        df.set_index("Cities", inplace=True)
         file_names.append(filename[filename.find('Q'):])
         saved_dataframes.append(df)
 
-    print(saved_dataframes)
-    print(file_names)
+    # print(saved_dataframes)
+    # print(file_names)
     return saved_dataframes, file_names
 
 
@@ -37,7 +40,7 @@ def save_data(file_name, fiscal_date):
     # Viewing available
     # sheets in it
     wks = xw.sheets
-    print("Available sheets :\n", wks)
+    # print("Available sheets :\n", wks)
 
     # Selecting a sheet
     ws = wks[0]
@@ -58,15 +61,17 @@ def save_data(file_name, fiscal_date):
     for city_index in city_cells:
         cities.append(ws.range(city_index).value)
 
+    attributes.append("Cities")
     for attribute_index in attribute_cells:
         attributes.append(ws.range(attribute_index).value)
 
-    df = pd.DataFrame(index=cities,
-                      columns=attributes)
-
+    df = pd.DataFrame(
+        columns=attributes)
+    df.set_index("Cities")
     for i in range(3, 68):
+        df.at[i-3, "Cities"] = ws.range(f'B{i}').value
         for j in 'CDEFGHIJKLMNOPRST':
-            df.at[ws.range(f'B{i}').value, ws.range(
+            df.at[i-3, ws.range(
                 f'{j}2').value] = ws.range(f'{j}{i}').value
 
     # city_values = ['D2']
@@ -75,9 +80,10 @@ def save_data(file_name, fiscal_date):
     #     city_points = city_cells[i][1:]
     #     total_points.append(ws.range(f'{city_values[0][0]}{city_points}').value)
 
-    print(df)
-
+    # print(df)
+    # df.set_index("Cities")
     df.to_csv(f'static/stored-data/{fiscal_date}.csv')
+    print(df)
 
     return df
 
@@ -95,11 +101,47 @@ def save_data(file_name, fiscal_date):
     # im.show()
 
 
+# Split dataframes into checked cities and attributes
+def split_dataframe(dataframes, cities, attributes):
+    df = dataframes[0]
+
+    df2 = df.loc[cities, attributes]
+
+    return df2
+
+
+# Called if there is only one dataframe used
+def make_single_bar_graph(df):
+    df.plot.bar()
+
+    plt.savefig('./static/images/graph.png', dpi=120, bbox_inches='tight')
+    im = Image.open('./static/images/graph.png')
+    im.show()
+
+# Called if there is only one dataframe used
+
+
+def make_single_line_graph(df):
+    df.plot()
+
+    plt.savefig('./static/images/graph.png', dpi=120, bbox_inches='tight')
+    im = Image.open('./static/images/graph.png')
+    im.show()
+
+
 file_names = []
 saved_dataframes = []
-#saved_dataframes, file_names = read_saved_data()
+saved_dataframes, file_names = read_saved_data()
 
-print(file_names)
-print(saved_dataframes)
+# print(file_names)
+# print(saved_dataframes)
 
-save_data('DummyData.xlsx', '4Q20')
+#df = save_data('DummyData.xlsx', '4QFY20')
+
+#print(df.loc["Atlanta", "CLIP"])
+
+
+df = split_dataframe(saved_dataframes, cities=[
+                     "Atlanta", "Baltimore"], attributes=["CLIP", "Total Points Earned"])
+
+make_single_line_graph(df)
