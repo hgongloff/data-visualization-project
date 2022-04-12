c2429c6a1349c1611935cad78fb427d1d33d7ccd
Hunter <hgongloff12@gmail.com>
2022-04-12 16:17:46 -0400

    Pie and Line graph now functional with front end

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from werkzeug.datastructures import FileStorage
 import pandas as pd
 import matplotlib
 from PIL import Image
-matplotlib.use('Agg')
+import glob
 
 UPLOAD_FOLDER = './data'
 ALLOWED_EXTENSIONS = {'xlsx'}
@@ -18,120 +18,270 @@ app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-attribute_names = []
 attribute_cells = []
 
-city_names = []
 city_cells = []
+
 graph_type = ""
 
 for i in range(3, 68):
     city_cells.append(f"B{i}")
 
-for i in 'CDEFGHIJKLMNOP':
+for i in 'CDEFGHIJKLMNOPRST':
     attribute_cells.append(f"{i}2")
 
 
-@app.route('/')
-def index():
-    return render_template("main.html", attribute_cells=attribute_cells, attribute_names=attribute_names,
-                           attribute_count=len(attribute_names),
-                           city_cells=city_cells, city_names=city_names, city_count=len(city_names))
-
-
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # print(request.form.getlist('City'))
-        # checked_cities = request.form.getlist('City')
-        # checked_attributes = request.form.getlist('Attribute')
-        # graph_type = request.form.get('graph_type')
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        city_names, attribute_names = load_data()
-        return render_template("main.html", attribute_cells=attribute_cells, attribute_names=attribute_names,
-                               attribute_count=len(attribute_names),
-                               city_cells=city_cells, city_names=city_names, city_count=len(city_names))
+def index():
+    clear_file = True
 
+    if request.method == 'POST':
+        print("Here is a post")
+        print(request.form.get('year'))
+        print(request.form.get('quarter'))
+        print(request.form.getlist('Dataframe'))
+        print(request.form.getlist('City'))
+        print(request.form.getlist('Attribute'))
+        quarter = request.form.get('quarter')
+        year = request.form.getlist('year')
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            print("Call file")
+            uploaded_file.save(uploaded_file.filename)
+            if int(year[0]) < 10:
+                save_data(uploaded_file.filename, f'FY0{year[0]}{quarter}')
+            else:
+                save_data(uploaded_file.filename, f'FY{year[0]}{quarter}')
+
+    saved_dataframes, excel_names = load_initial_excel_files()
+    print(excel_names)
+    uploaded_file = ''
+
+    attribute_names = []
+    city_names = []
+
+    if saved_dataframes:
+        attribute_names, city_names = load_initial_cities_attributes(saved_dataframes)
+
+    new_excel_names = []
+    for name in excel_names:
+        new_excel_names.append(name[0:6])
+    excel_names = new_excel_names
+
+    return render_template("main.html", attribute_names=attribute_names, clear_file=1,
+                           attribute_count=len(attribute_names), city_names=city_names, city_count=len(city_names),
+                           excel_names=excel_names, excel_count=len(excel_names))
 
 @app.route('/graph', methods=['GET', 'POST'])
-def create_graph():
+def graph_page():
+    fiscal_years = []
     if request.method == 'POST':
-        checked_attributes = []
-        checked_cities = []
+        print("Here is a post")
+        print(request.form.get('year'))
+        print(request.form.getlist('quarter'))
+        print(request.form.getlist('Dataframe'))
         print(request.form.getlist('City'))
-        checked_cities = request.form.getlist('City')
-        checked_attributes = request.form.getlist('Attribute')
-        graph_type = request.form.get('graph_type')
         print(request.form.getlist('Attribute'))
-        print(request.form.get('graph_type'))
-        city_names, attribute_names = load_data()
-        save_graph(graph_type, checked_cities, checked_attributes)
-        # checked_attributes.clear()
-        # checked_cities.clear()
-        return render_template("show_graph.html", attribute_cells=attribute_cells, attribute_names=attribute_names,
-                               attribute_count=len(attribute_names),
-                               city_cells=city_cells, city_names=city_names, city_count=len(city_names))
+        print(request.form.getlist('graph_type'))
+        print("Here")
+        cities = request.form.getlist('City')
+        attributes = request.form.getlist('Attribute')
+        fiscal_years = request.form.getlist('Dataframe')
+        fiscal_years = split_fy(fiscal_years)
+        graph_type = request.form.get('graph_type')
+        saved_dataframes, excel_names = load_initial_excel_files()
+
+        if len(attributes) and len(cities) and len(fiscal_years):
+            df_list = split_dataframe(saved_dataframes, fiscal_years, cities=cities, attributes=attributes)
+            if graph_type == 'Line':
+                print(graph_type)
+                make_line_graph(df_list, cities, attributes, fiscal_years)
+            elif graph_type == 'Pie':
+                make_pie_chart(df_list, cities, attributes, fiscal_years)
+
+    saved_dataframes, excel_names = load_initial_excel_files()
+
+    if saved_dataframes:
+        attribute_names, city_names = load_initial_cities_attributes(saved_dataframes)
+
+    new_excel_names = []
+    for name in excel_names:
+        new_excel_names.append(name[0:6])
+    excel_names = new_excel_names
+
+    return render_template("main.html", attribute_names=attribute_names, clear_file=1,
+                        attribute_count=len(attribute_names), city_names=city_names, city_count=len(city_names),
+                        excel_names=excel_names, excel_count=len(excel_names))
 
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template("user.html", username=name)
 
+def load_initial_excel_files():
+    _saved_dataframes = []
+    _file_names = []
 
-def load_data():
-    wb = xw.Book('DummyData.xlsx')
+    path = r'static/stored-data'  # use your path
+    all_files = glob.glob(path + "/*.csv")
+    for filename in all_files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        _file_names.append(filename[filename.find('F'):])
+        _saved_dataframes.append(df)
+
+    # print(saved_dataframes)
+    # print(file_names)
+    return _saved_dataframes, _file_names
+
+# Save Data from recently gotten excel file
+def save_data(file_name, fiscal_date):
+
+    wb = xw.Book(f'{file_name}')
 
     # Viewing available
     # sheets in it
     wks = xw.sheets
-    print("Available sheets :\n", wks)
+    # print("Available sheets :\n", wks)
 
     # Selecting a sheet
     ws = wks[0]
 
-    city_names = ws.range("B3:B67").value
-    attribute_names = ws.range("C2:P2").value
-
-    wb.close()
-
-    return city_names, attribute_names
-
-
-def save_graph(graph_type, checked_cities, checked_attributes):
-    wb = xw.Book('DummyData.xlsx')
-    total_points = []
+    city_cells = []
+    attribute_cells = []
     cities = []
+    attributes = []
+    points = [[]]
+    total_points = []
 
-    # Viewing available
-    # sheets in it
-    wks = xw.sheets
-    print("Available sheets :\n", wks)
+    for i in range(3, 68):
+        city_cells.append(f"B{i}")
 
-    # Selecting a sheet
-    ws = wks[0]
+    for i in 'CDEFGHIJKLMNOPRST':
+        attribute_cells.append(f"{i}2")
 
-    for city in checked_cities:
-        cities.append(ws.range(city).value)
-    print(cities)
-    print(checked_cities)
-    for i in range(0, len(checked_cities)):
-        city_points = checked_cities[i][1:]
-        total_points.append(
-            ws.range(f'{checked_attributes[0][0]}{city_points}').value)
-    print(cities)
-    print(total_points)
-    plt.clf()
-    plt.bar(cities, total_points)
-    plt.title(f'Cities vs {ws.range(checked_attributes[0]).value}')
-    plt.xlabel('Cities')
-    plt.ylabel(f'{ws.range(checked_attributes[0]).value}')
-    # Dynamically generate whatever name the user sends
-    plt.savefig('./static/images/graph.png')
+    for city_index in city_cells:
+        cities.append(ws.range(city_index).value)
+
+    attributes.append("Cities")
+    for attribute_index in attribute_cells:
+        attributes.append(ws.range(attribute_index).value)
+
+    df = pd.DataFrame(
+        columns=attributes)
+    
+    for i in range(3, 68):
+        df.at[i-3, "Cities"] = ws.range(f'B{i}').value
+        for j in 'CDEFGHIJKLMNOPRST':
+            df.at[i-3, ws.range(
+                f'{j}2').value] = ws.range(f'{j}{i}').value
+
+    # city_values = ['D2']
+
+    # for i in range(0, len(city_cells)):
+    #     city_points = city_cells[i][1:]
+    #     total_points.append(ws.range(f'{city_values[0][0]}{city_points}').value)
+
+    # print(df)
+    # df.set_index("Cities")
+    df.to_csv(f'static/stored-data/{fiscal_date}.csv')
+    #print(df)
+    xl = xw.apps.active.api
+    xl.Quit()
+
+    return df
+
+def load_initial_cities_attributes(saved_dataframes):
+    _attribute_names = saved_dataframes[0].columns.drop(["Unnamed: 0", "Cities"])
+    _city_names = []
+    for i in range(65):
+        _city_names.append(saved_dataframes[0]['Cities'][i])
+    return _attribute_names, _city_names
+
+def load_new_excel_file():
+    return
+
+# Split up quarter year string
+def split_fy(fy_list):
+    new_list = []
+    for fy in fy_list:
+        new_list.append(fy[0:6])
+
+    return new_list
+
+# Split dataframes into checked cities and attributes
+def split_dataframe(dataframes, fiscal_years, cities, attributes):
+    df_list = []
+    for df in dataframes:
+        df.set_index("Cities", inplace=True)
+        df_list.append(df.loc[cities, attributes])
+        #print("done")
+    
+    for i in range(len(fiscal_years)):
+        df_list[i]['FYQ'] = fiscal_years[i]
+        #print(df_list[i])
+
+    #df2 = df.loc[cities, attributes]
+    print("Split it")
+    print(df_list[0])
+    return df_list
+
+
+def make_line_graph(df_list, cities, attributes, fiscal_years):
+
+    df_total = pd.concat(df_list)
+
+    df_total = df_total[df_total['FYQ'].isin(fiscal_years)]
+
+    print("Here it is")
+    print(df_total)
+
+    df_total = df_total.sort_values("FYQ")
+
+    #df_total = df_total.groupby(['Cities'])
+
+    #df_total.plot(x='FYQ', y=attributes)
+    #df_total.pivot(index='FYQ', columns=attributes, values=attributes).plot()
+
+    city_att = []
+
+    city_labels = []
+
+    fig, ax = plt.subplots()
+    
+    for city, gp in df_total.groupby('Cities'):
+        for i in range(len(attributes)):
+            city_att.append(f'{city}: {attributes[i]}')
+        city_labels.append(city_att)
+        city_att = []
+     
+    i = 0
+    for city, gp in df_total.groupby('Cities'):
+        gp.plot(x='FYQ', y=attributes, ax=ax, label=city_labels[i])
+        i = i + 1
+
+    
+    plt.savefig('./static/images/graph.png', dpi=200, bbox_inches='tight')
     im = Image.open('./static/images/graph.png')
     im.show()
 
-    # checked_attributes.clear()
-    # checked_cities.clear()
 
-    wb.close()
+def make_pie_chart(df_list, cities, attributes, fiscal_years):
+
+    df_total = pd.concat(df_list)
+
+    df_total = df_total[df_total['FYQ'].isin(fiscal_years)]
+
+    print("Here it is")
+    print(df_total)
+
+    df_total = df_total.sort_values("FYQ")
+    titles=df_total['FYQ'].to_list()
+    df_total = df_total.drop('FYQ', 1)
+
+    print(type(df_total))
+
+
+
+    df_total.T.plot.pie(subplots=True, figsize=(20, 3), legend=False, title=titles)
+
+    
+    plt.savefig('./static/images/graph.png', dpi=200)
+    im = Image.open('./static/images/graph.png')
+    im.show()
